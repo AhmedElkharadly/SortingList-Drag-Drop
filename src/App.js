d3ae789219451b31cea3fb91a311af4cc9d9234c
AhmedElkharadly <ahmed.e.elkharadly@gmail.com>
2023-03-15 17:01:06 +0200

    README

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import DragableItems from "./Components/DragableList/DragableList";
-// import SortableList from './Components/SortableList';
-// import { items } from './Components/data';
 
 function App() {
   return <DragableItems />;
